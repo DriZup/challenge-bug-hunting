eb98e8e41abe44bee48a9e81d9f3a2e29c4142c2
adriana.nogueira <adriana.nogueira@zup.com.br>
2024-12-30 08:37:46 -0300

    feat: validar dados antes de adicionar vídeo.

--- src/main/service/VideoServiceImpl.java
+++ src/main/service/VideoServiceImpl.java
@@ -14,11 +14,33 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     public void addVideo(Video video) {
+        validarVideo(video);
         repository.save(video);
     }
 
     @Override
     public List<Video> listVideos() {
         return repository.findAll();
+    }
+
+    private static void validarVideo(Video video){
+        if(video == null){
+            throw new IllegalArgumentException("O vídeo não pode ser nulo!");
+        }
+        if(video.getTitulo() == null){
+            throw new IllegalArgumentException("O título do vídeo não pode ser nulo!");
+        }
+        if(video.getDescricao() == null){
+            throw new IllegalArgumentException("A descrição do vídeo não pode ser nula!");
+        }
+        if(video.getDuracao() <= 0 ){
+            throw new IllegalArgumentException("A duração do vídeo deve ser um número positivo!");
+        }
+        if(video.getCategoria() == null){
+            throw new IllegalArgumentException("A categoria do vídeo não pode ser nula!");
+        }
+        if(video.getDataPublicacao() == null){
+            throw new IllegalArgumentException("A Data do vídeo não pode ser nula!");
+        }
     }
 }
