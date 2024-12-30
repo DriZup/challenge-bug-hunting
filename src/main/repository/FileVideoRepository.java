dec13622c8fda97fa9f56810aa01fa072c3d8735
adriana.nogueira <adriana.nogueira@zup.com.br>
2024-12-30 09:33:09 -0300

    refactor: delegar operações de arquivo para a classe FileHandler

--- src/main/repository/FileVideoRepository.java
+++ src/main/repository/FileVideoRepository.java
@@ -1,48 +1,33 @@
 package main.repository;
 
 import main.model.Video;
+import main.util.FileHandler;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileVideoRepository implements VideoRepository {
-    private final File file;
+    private final FileHandler fileHandler;
 
-    public FileVideoRepository(String filePath) {
-        this.file = new File(filePath);
-        try {
-            if (file.createNewFile()) {
-                System.out.println("Arquivo criado: " + file.getName());
-            }
-        } catch (IOException e) {
-            System.out.println("Error ao criar o arquivo: " + e.getMessage());
-        }
+    public FileVideoRepository(String filePath, FileHandler fileHandler) {
+        this.fileHandler = fileHandler;
     }
 
     @Override
     public void save(Video video) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
-            bw.write(video.toString());
-            bw.newLine();
-        } catch (IOException e) {
-            System.out.println("Error ao salvar Vídeo: " + e.getMessage());
-        }
+        fileHandler.writeLine(video.toString());
     }
 
     @Override
     public List<Video> findAll() {
         List<Video> videos = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                Video video = Video.fromString(line);
-                if (video != null) {
-                    videos.add(video);
-                }
+        List<String> lines = fileHandler.readAllLines();
+        for (String line : lines) {
+            Video video = Video.fromString(line);
+            if (video != null) {
+                videos.add(video);
             }
-        } catch (IOException e) {
-            System.out.println("Error ao ler vídeos: " + e.getMessage());
         }
         return videos;
     }
